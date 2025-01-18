c840c9986684dbb9a243248ded2687276944f0d4
Swetha <swethamurugesh1@gmail.com>
2025-01-18 20:06:04 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from "react-dom/client"
+import one from "./assests/images/one.jpg"
+import two from "./assests/images/two.jpg"
+import three  from "./assests/images/three.jpg"
+import four from "./assests/images/four.jpg"
+import five from "./assests/images/five.jpg"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import six from "./assests/images/six.jpg"
+import "./style2.css"
+
+const root = ReactDom.createRoot(document.getElementById("root"))
+
+function Images() {
+  return (
+    <div className="container">
+      <div className="one">
+        <img src={one} alt="one"  style={{width:"100%",height:"90%"}}></img>
+        <p>Sunflower</p>
+
+      </div>
+      <div className="one">
+        <img src={two} alt="one"  style={{width:"100%",height:"90%"}}></img>
+        <p>Cherry blossom</p>
+
+      </div>
+      <div className="one">
+        <img src={three} alt="one"  style={{width:"100%",height:"90%"}}></img>
+        <p>Rose plant</p>
+
+      </div>
+      <div className="one">
+        <img src={four} alt="one"  style={{width:"100%",height:"90%"}}></img>
+        <p> Red Rose </p>
+
+      </div>
+      <div className="one">
+        <img src={one} alt="one"  style={{width:"100%",height:"90%"}}></img>
+        <p>Sunflower</p>
+
+      </div>
+      <div className="one">
+        <img src={four} alt="one"  style={{width:"100%",height:"90%"}}></img>
+        <p> Red Rose</p>
+
+      </div>
+      <div className="one">
+        <img src={six} alt="one"  style={{width:"100%",height:"90%"}}></img>
+        <p>Daisy</p>
+
+      </div>
+      <div className="one">
+        <img src={five} alt="one"  style={{width:"100%",height:"90%"}}></img>
+        <p>Tulips</p>
+
+      </div>
+      
+
+
+    </div>
+  )
+}
+
+
+
+root.render(<Images></Images>)
